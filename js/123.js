a4336787ff53d7df5c8a14f4ecb42c98735f15f5
lichiehwang <131224887+lichiehwang@users.noreply.github.com>
2023-11-29 13:41:07 +0900

    Add files via upload

---
+++ js/123.js
@@ -0,0 +1,116 @@
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+
+const slide = document.getElementById('slide');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const indicator = document.getElementById('indicator');
+const lists = document.querySelectorAll('.list');
+const totalSlides = lists.length;
+let count = 0;
+let autoPlayInterval;
+function updateListBackground() {
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].style.backgroundColor = i === count % totalSlides ? '#DEB283' : '#fff';
+  }
+}
+function nextClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count++;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+}
+function prevClick() {
+  slide.classList.remove(`slide${count % totalSlides + 1}`);
+  count--;
+  if (count < 0) count = totalSlides - 1;
+  slide.classList.add(`slide${count % totalSlides + 1}`);
+  updateListBackground();
+}
+function startAutoPlay() {
+  autoPlayInterval = setInterval(nextClick, 5000);
+}
+function resetAutoPlayInterval() {
+  clearInterval(autoPlayInterval);
+  startAutoPlay();
+}
+next.addEventListener('click', () => {
+  nextClick();
+  resetAutoPlayInterval();
+});
+prev.addEventListener('click', () => {
+  prevClick();
+  resetAutoPlayInterval();
+});
+indicator.addEventListener('click', (event) => {
+  if (event.target.classList.contains('list')) {
+    const index = Array.from(lists).indexOf(event.target);
+    slide.classList.remove(`slide${count % totalSlides + 1}`);
+    count = index;
+    slide.classList.add(`slide${count % totalSlides + 1}`);
+    updateListBackground();
+    resetAutoPlayInterval();
+  }
+});
+startAutoPlay();
+
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+  $(function () {
+    var webStorage = function () {
+      if (sessionStorage.getItem('access')) {
+        /*
+          2回目以降アクセス時の処理
+        */
+        $(".loading").addClass('is-active');
+      } else {
+        /*
+          初回アクセス時の処理
+        */
+        sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
+        $(".loading-animation").addClass('is-active'); // loadingアニメーションを表示
+        setTimeout(function () {
+          // ローディングを数秒後に非表示にする
+          $(".loading").addClass('is-active');
+          $(".loading-animation").removeClass('is-active');
+        }, 3000); // ローディングを表示する時間
+      }
+    }
+    webStorage();
+  });
+  
